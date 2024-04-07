0158292235af01967738649b2705d0d439f5893e
Niranjan H <43512321+niranjanh123@users.noreply.github.com>
2024-04-07 19:25:19 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop and remove all the running containers (if any)
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+sudo docker stop $(docker ps -a -q)
+sudo docker rm $(docker ps -a -q)
